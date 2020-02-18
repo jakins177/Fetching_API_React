ad4b672c0ceb844cfd867a0ac9d8c94e33f5414a
Student <student@PSXGA125s-MacBook-Air.local>
2020-02-18 17:15:43 -0500

    working example of fetching name from an API and displaying it to the DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default  class App extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          data: [],
+        };
+
+      }
+
+    componentDidMount(){
+      fetch('https://picsum.photos/v2/list?page=2&limit=20')
+        .then((response) => { 
+          return response.json(); 
+        })
+        .then((data) => {
+          // console.log(data);
+          this.setState(  { data }  )
+        })
+      }
+
+      render() {
+        
+        return(
+          <div>
+            {
+              this.state.data.map(same => {
+                return <div>{same.author}</div>
+              })
+            }
+          </div>
+        )
+      }
+
+
 }
 
-export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
