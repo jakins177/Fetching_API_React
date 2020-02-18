d19ab68f19e560b15a1277ee5f2ea21e96e0706c
Student <student@PSXGA125s-MacBook-Air.local>
2020-02-18 17:17:42 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,64 +1,44 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 //import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
-export default  class App extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          data: [],
-        };
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
 
-      }
+  }
 
-    componentDidMount(){
-      fetch('https://picsum.photos/v2/list?page=2&limit=20')
-        .then((response) => { 
-          return response.json(); 
+  componentDidMount() {
+    fetch('https://picsum.photos/v2/list?page=2&limit=20')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        this.setState({
+          data
         })
-        .then((data) => {
-          // console.log(data);
-          this.setState(  { data }  )
-        })
-      }
+      })
+  }
 
-      render() {
-        
-        return(
-          <div>
-            {
-              this.state.data.map(same => {
-                return <div>{same.author}</div>
-              })
-            }
-          </div>
-        )
-      }
+  render() {
+
+    return ( <div > {
+        this.state.data.map(same => {
+          return <div > {
+            same.author
+          } </div>
+        })
+      } </div>
+    )
+  }
 
 
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
